ab62f051f3cb2dc580a395fd55e730c2fe3a6c52
isharma-sharma <isharma.sharma55@gmail.com>
2017-08-30 10:03:29 -0400

    initializing git repo

---
+++ db/migrate/20170830130432_add_name_age_and_address_to_users.rb
@@ -0,0 +1,7 @@
+class AddNameAgeAndAddressToUsers < ActiveRecord::Migration[5.0]
+  def change
+    add_column :users, :name, :string
+    add_column :users, :age, :string
+    add_column :users, :address, :string
+  end
+end
